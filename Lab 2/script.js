4cf0f2457d21aea010c1b9614fa75e823fe31dff
Jibran Syed <jibran.syed77@gmail.com>
2025-10-02 14:28:12 -0400

    lab 2 activity 3 complete

--- Lab 2/script.js
+++ Lab 2/script.js
@@ -9,3 +9,28 @@ function validateSearchForm() {
         return true;
     }
 }
+
+function calculate(){
+    let op1 = Number(document.forms["simpleCalculator"]["operand1"].value);
+    let oper = document.forms["simpleCalculator"]["operator"].value;
+    let op2 = Number(document.forms["simpleCalculator"]["operand2"].value);
+    let result = 0;
+    switch(oper){
+        case '+':
+            result = op1 + op2;
+            break;
+        case '-':
+            result = op1 - op2;
+            break;
+        case '*':
+            result = op1 * op2;
+            break;
+        case '/':
+            result = op1 / op2;
+            break;
+        default:
+            alert("Invalid operator");
+            return;
+    }
+    document.forms["simpleCalculator"]["result"].value = result;
+}
