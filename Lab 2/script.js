e3e3a57a86977c3de445e2422736fd7c2ff9cc56
Jibran Syed <jibran.syed77@gmail.com>
2025-10-02 15:12:59 -0400

    finished lab 2

--- Lab 2/script.js
+++ Lab 2/script.js
@@ -15,22 +15,55 @@ function calculate(){
     let oper = document.forms["simpleCalculator"]["operator"].value;
     let op2 = Number(document.forms["simpleCalculator"]["operand2"].value);
     let result = 0;
-    switch(oper){
-        case '+':
-            result = op1 + op2;
-            break;
-        case '-':
-            result = op1 - op2;
-            break;
-        case '*':
-            result = op1 * op2;
-            break;
-        case '/':
-            result = op1 / op2;
-            break;
-        default:
-            alert("Invalid operator");
-            return;
+
+    if(isNaN(op1) || isNaN(op2)){
+        alert("Please enter valid numbers for operands");
+
+    } else if(op1 == "" || op2 == ""){
+        alert("Please enter a Value for the operands");
+        
+    } else {
+        switch(oper){
+            case '+':
+                result = op1 + op2;
+                break;
+            case '-':
+                result = op1 - op2;
+                break;
+            case '*':
+                result = op1 * op2;
+                break;
+            case '/':
+                if(op2 == 0){
+                    alert("Division by zero is not allowed");
+                    return;
+                }
+                result = op1 / op2;
+                break;
+            default:
+                alert("Invalid operator");
+                return;
+        }
+        document.forms["simpleCalculator"]["result"].value = result;
     }
-    document.forms["simpleCalculator"]["result"].value = result;
+}
+
+function dis(val, formName) {
+    document.forms[formName]["result"].value += val;    
+}
+
+function clr(formName) {
+    document.forms[formName]["result"].value = "";    
+}
+
+function solve(formName) {
+    let x = document.forms[formName]["result"].value;
+    let y = math.evaluate(x);
+    document.forms[formName]["result"].value = x + " = " + y;
+}
+
+function delChar(formName) {
+    const form = document.forms[formName];
+    const resultBox = form.result;
+    resultBox.value = resultBox.value.slice(0, -1);
 }
